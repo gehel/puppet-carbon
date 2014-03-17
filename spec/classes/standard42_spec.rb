febed8a25bea18c055d5559ffdcee6ed67035a3c
gehel <guillaume.lederrey@gmail.com>
2014-03-17 23:17:33 +0100

    try to fix some tests

--- spec/classes/standard42_spec.rb
+++ spec/classes/standard42_spec.rb
@@ -7,67 +7,67 @@ describe 'carbon' do
   let(:facts) { { :ipaddress => '10.42.42.42' } }
 
   describe 'Test standard installation' do
-    it { should contain_package('carbon').with_ensure('present') }
-    it { should contain_service('carbon').with_ensure('running') }
-    it { should contain_service('carbon').with_enable('true') }
+    it { should contain_package('graphite-carbon').with_ensure('present') }
+    it { should contain_service('carbon-cache').with_ensure('running') }
+    it { should contain_service('carbon-cache').with_enable('true') }
     it { should contain_file('carbon.conf').with_ensure('present') }
   end
 
   describe 'Test installation of a specific version' do
     let(:params) { {:version => '1.0.42' } }
-    it { should contain_package('carbon').with_ensure('1.0.42') }
+    it { should contain_package('graphite-carbon').with_ensure('1.0.42') }
   end
 
   describe 'Test standard installation with monitoring and firewalling' do
-    let(:params) { {:monitor => true , :firewall => true, :port => '42', :protocol => 'tcp' } }
-    it { should contain_package('carbon').with_ensure('present') }
-    it { should contain_service('carbon').with_ensure('running') }
-    it { should contain_service('carbon').with_enable('true') }
+    let(:params) { {:monitor => true , :firewall => true, :port => '2003', :protocol => 'tcp' } }
+    it { should contain_package('graphite-carbon').with_ensure('present') }
+    it { should contain_service('carbon-cache').with_ensure('running') }
+    it { should contain_service('carbon-cache').with_enable('true') }
     it { should contain_file('carbon.conf').with_ensure('present') }
     it { should contain_monitor__process('carbon_process').with_enable('true') }
-    it { should contain_firewall('carbon_tcp_42').with_enable('true') }
+    it { should contain_firewall('carbon_tcp_2003').with_enable('true') }
   end
 
   describe 'Test decommissioning - absent' do
-    let(:params) { {:absent => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it 'should remove Package[carbon]' do should contain_package('carbon').with_ensure('absent') end
-    it 'should stop Service[carbon]' do should contain_service('carbon').with_ensure('stopped') end
-    it 'should not enable at boot Service[carbon]' do should contain_service('carbon').with_enable('false') end
+    let(:params) { {:absent => true, :monitor => true , :firewall => true, :port => '2003', :protocol => 'tcp'} }
+    it 'should remove Package[graphite-carbon]' do should contain_package('graphite-carbon').with_ensure('absent') end
+    it 'should stop Service[carbon-cache]' do should contain_service('carbon-cache').with_ensure('stopped') end
+    it 'should not enable at boot Service[carbon]' do should contain_service('carbon-cache').with_enable('false') end
     it 'should remove carbon configuration file' do should contain_file('carbon.conf').with_ensure('absent') end
     it { should contain_monitor__process('carbon_process').with_enable('false') }
-    it { should contain_firewall('carbon_tcp_42').with_enable('false') }
+    it { should contain_firewall('carbon_tcp_2003').with_enable('false') }
   end
 
   describe 'Test decommissioning - disable' do
-    let(:params) { {:disable => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('carbon').with_ensure('present') }
-    it 'should stop Service[carbon]' do should contain_service('carbon').with_ensure('stopped') end
-    it 'should not enable at boot Service[carbon]' do should contain_service('carbon').with_enable('false') end
+    let(:params) { {:disable => true, :monitor => true , :firewall => true, :port => '2003', :protocol => 'tcp'} }
+    it { should contain_package('graphite-carbon').with_ensure('present') }
+    it 'should stop Service[carbon]' do should contain_service('carbon-cache').with_ensure('stopped') end
+    it 'should not enable at boot Service[carbon]' do should contain_service('carbon-cache').with_enable('false') end
     it { should contain_file('carbon.conf').with_ensure('present') }
     it { should contain_monitor__process('carbon_process').with_enable('false') }
-    it { should contain_firewall('carbon_tcp_42').with_enable('false') }
+    it { should contain_firewall('carbon_tcp_2003').with_enable('false') }
   end
 
   describe 'Test decommissioning - disableboot' do
-    let(:params) { {:disableboot => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('carbon').with_ensure('present') }
-    it { should_not contain_service('carbon').with_ensure('present') }
-    it { should_not contain_service('carbon').with_ensure('absent') }
-    it 'should not enable at boot Service[carbon]' do should contain_service('carbon').with_enable('false') end
+    let(:params) { {:disableboot => true, :monitor => true , :firewall => true, :port => '2003', :protocol => 'tcp'} }
+    it { should contain_package('graphite-carbon').with_ensure('present') }
+    it { should_not contain_service('carbon-cache').with_ensure('present') }
+    it { should_not contain_service('carbon-cache').with_ensure('absent') }
+    it 'should not enable at boot Service[carbon]' do should contain_service('carbon-cache').with_enable('false') end
     it { should contain_file('carbon.conf').with_ensure('present') }
     it { should contain_monitor__process('carbon_process').with_enable('false') }
-    it { should contain_firewall('carbon_tcp_42').with_enable('true') }
+    it { should contain_firewall('carbon_tcp_2003').with_enable('true') }
   end
 
   describe 'Test noops mode' do
-    let(:params) { {:noops => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
-    it { should contain_package('carbon').with_noop('true') }
-    it { should contain_service('carbon').with_noop('true') }
+    let(:params) { {:noops => true, :monitor => true , :firewall => true, :port => '2003', :protocol => 'tcp'} }
+    it { should contain_package('graphite-carbon').with_noop('true') }
+    it { should contain_service('carbon-cache').with_noop('true') }
     it { should contain_file('carbon.conf').with_noop('true') }
     it { should contain_monitor__process('carbon_process').with_noop('true') }
     it { should contain_monitor__process('carbon_process').with_noop('true') }
-    it { should contain_monitor__port('carbon_tcp_42').with_noop('true') }
-    it { should contain_firewall('carbon_tcp_42').with_noop('true') }
+    it { should contain_monitor__port('carbon_tcp_2003').with_noop('true') }
+    it { should contain_firewall('carbon_tcp_2003').with_noop('true') }
   end
 
   describe 'Test customizations - template' do
@@ -118,38 +118,38 @@ describe 'carbon' do
   end
 
   describe 'Test Firewall Tools Integration' do
-    let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
-    it { should contain_firewall('carbon_tcp_42').with_tool('iptables') }
+    let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "2003" } }
+    it { should contain_firewall('carbon_tcp_2003').with_tool('iptables') }
   end
 
   describe 'Test OldGen Module Set Integration' do
-    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
+    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "2003" , :protocol => 'tcp' } }
     it { should contain_monitor__process('carbon_process').with_tool('puppi') }
-    it { should contain_firewall('carbon_tcp_42').with_tool('iptables') }
+    it { should contain_firewall('carbon_tcp_2003').with_tool('iptables') }
     it { should contain_puppi__ze('carbon').with_ensure('present') }
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
+    let(:params) { { :port => '2003' } }
     it 'should honour top scope global vars' do should contain_monitor__process('carbon_process').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :carbon_monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
+    let(:params) { { :port => '2003' } }
     it 'should honour module specific vars' do should contain_monitor__process('carbon_process').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :carbon_monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
+    let(:params) { { :port => '2003' } }
     it 'should honour top scope module specific over global vars' do should contain_monitor__process('carbon_process').with_enable('true') end
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
+    let(:params) { { :monitor => true , :firewall => true, :port => '2003' } }
     it 'should honour passed params over global vars' do should contain_monitor__process('carbon_process').with_enable('true') end
   end
 
